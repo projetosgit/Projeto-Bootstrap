1f128c4816e09c44418b76d3e41df16006e043be
Artur da Silva Rezende <arturrezende4@gmail.com>
2020-02-06 20:50:06 -0300

    Novas funcionalidades foram implementadas no projeto

--- js/projeto_bootstrap.js
+++ js/projeto_bootstrap.js
@@ -3,6 +3,27 @@
 * @author Artur
 */
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+/* ============= Jumbotron select ============= */
+
+var select = document.querySelector(".select-promocoes"); //Essa linha controla os itens selecionados
+var img_jumbotrom = document.querySelector(".img-jumbotron"); //Essa linha controla as imagens do jumbotron
+
+var item_selecionado = document.querySelector(".promocoes"); //Essa linha controla o botão de ação
+item_selecionado.addEventListener("click", select_promocao); //Essa linha vai execultar o método select_promocao
+
+function select_promocao(){
+	
+	if (select.options[select.selectedIndex].value == 1){
+		img_jumbotrom.src = "img/jumbotron-5.png";
+	} else if(select.options[select.selectedIndex].value == 2){
+		img_jumbotrom.src = "img/jumbotron-2.png";
+	} else if(select.options[select.selectedIndex].value == 3){
+		img_jumbotrom.src = "img/jumbotron-3.png";
+	} else if(select.options[select.selectedIndex].value == 4){
+		img_jumbotrom.src = "img/jumbotron-4.png";
+	} else if(select.options[select.selectedIndex].value == 5){
+		img_jumbotrom.src = "img/jumbotron-1.png";
+	} else {
+		alert("Escolha uma opção");
+	}
+}
